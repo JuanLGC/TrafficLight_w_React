48733cc4a1c7cfe1522ed7279e7d2390959eec18
JuanLGC <juangcardinale@gmail.com>
2020-08-04 09:05:34 +0000

    styles added

--- src/js/component/trafficLight.js
+++ src/js/component/trafficLight.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 export default function TrafficLight(color) {
-	const [light, stateChange] = useState(false);
-	let lightColor = "";
+	//Declaracion de variables: (1) cambia el estado entre verdadero y falso
+	//                          (2) Determina el nombre de la className en CSS que cambiara el color cuando cambie el estado
+	const [light, stateChange] = useState(false); // (1)
+	let lightColor = ""; // (2)
+
+	//condicional que hace el cambio del classname usando variable (2) y el estado de variable (1)
 
 	if (light === true) {
 		lightColor = color.color;
@@ -11,8 +15,10 @@ export default function TrafficLight(color) {
 		lightColor = "Light";
 	}
 
+	// Retorna el html de una luz que cambia de estado (true/false) al clickar en ella.
+
 	return (
-		<div className="row p-2 divisor">
+		<div className="row px-3 py-4 divisor">
 			<div
 				className={lightColor}
 				onClick={() => {
